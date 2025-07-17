72d74d4c015e749eb52ec8a67fdd120fd49a74bf
enxebre <alberto.garcial@hotmail.com>
2025-07-17 16:42:21 +0200

    Enable MutatingAdmissionPolicy API in KAS when feature gate is set (sync with upstream registration)

--- control-plane-operator/controllers/hostedcontrolplane/v2/kas/config.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/kas/config.go
@@ -239,6 +239,9 @@ func generateConfig(p KubeAPIServerConfigParams) (*kcpv1.KubeAPIServerConfig, er
 		if gate == "ValidatingAdmissionPolicy=true" {
 			runtimeConfig = append(runtimeConfig, "admissionregistration.k8s.io/v1beta1=true")
 		}
+		if gate == "MutatingAdmissionPolicy=true" {
+			runtimeConfig = append(runtimeConfig, "admissionregistration.k8s.io/v1alpha1=true")
+		}
 		if gate == "DynamicResourceAllocation=true" {
 			runtimeConfig = append(runtimeConfig, "resource.k8s.io/v1beta1=true")
 		}
